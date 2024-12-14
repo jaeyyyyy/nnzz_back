f7a78b4b579a8807371638a51d797bff2eef2c5a
김재연 <kpark9700@gmail.com>
2024-12-15 04:44:14 +0900

    [수정] 카드 생성 시 response 수정

--- src/main/java/com/nnzz/nnzz/dto/ShowCardDTO.java
+++ src/main/java/com/nnzz/nnzz/dto/ShowCardDTO.java
@@ -16,8 +16,21 @@ import java.util.List;
 public class ShowCardDTO {
     private int cardId;
     private int userId;
-    List<FoodTypeDTO> foodTypes; // 카테고리 관련
-    List<CardStoreDTO> stores; // 가게 관련
+    // List<FoodTypeDTO> foodTypes; // 카테고리 관련
+
+    // 카테고리
+    private int foodTypeId;
+    private String category;
+    private String description;
+
+    // List<CardStoreDTO> stores; // 가게 관련
+
+    // 가게
+    private String storeId;
+    private String name;
+    private String address;
+    private List<MenuDTO> menus; // 메뉴 정보
+
     private LocalDate cardDate; // 날짜 정보
     private String mealtime; // 점심, 저녁
     private Timestamp createdAt; // 생성한 날짜
